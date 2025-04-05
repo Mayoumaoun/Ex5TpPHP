97cb94b5dcc844b5e8945e60e6e4e2924b8c2c69
Mayoumaoun <oumaymaounissi2004@gmail.com>
2025-04-05 16:06:42 +0200

    ajout du datatable du sections

--- sections.php
+++ sections.php
@@ -1,46 +1,81 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
 
 <?php
+ob_start(); 
 include("classes/section.php");
 $sect=new Section();
-$listeSections=$sect->findAll();
 include("header.php");
-$role=$_SESSION["user"]["role"];
+if (isset($_GET['ajax'])) {
+    header('Content-Type: application/json');
+
+    $listeSections=$sect->findAll();
+    
+    if (!$listeSections) {
+        echo json_encode(["error" => "No sections found"]);
+        exit();
+    }
+
+    foreach ($listeSections as &$section) {
+        $id = $section->id;
+
+        $actions = "<a href='utils/liste.php?id={$id}&type=student'><i class='bi bi-list'></i></a> ";
+        
+        $section->actions = $actions; 
+    }
+
+    ob_end_clean(); 
+    echo json_encode(array_values($listeSections), JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+    exit();
+}
 ?>
         <br>
         <div class=" alert alert-light" role="alert"> List of students</div>
         <div class="container">
         
-        <div class="export"></div>
-        
-        <table class="table">
-            <thead>
-                <tr><td>id</td><td>designation</td><td>description</td><td>Actions</td></tr>
-            </thead>
-            <div class="container">
-            <div class="export">
+        <div class="export">
             <button onclick="window.location.href='export_csv_section.php'">Export CSV</button>
             <button onclick="window.location.href='export_excel_section.php'">Export Excel</button>
             <button onclick="window.location.href='export_pdf_section.php'">Export PDF</button>
             </div>
-            </div>
             <br>
-            <tbody>
-                <?php 
-                foreach($listeSections as $stud){
-                    echo "<tr>";
-                    foreach($stud as $key => $val){
-                        echo "<td>".$val."</td>";
-                    }
-                    echo "<td><a href='utils/liste.php?id=".$stud->id."&type=section'><i class='bi bi-list'></i></a> ";
-                    echo "</td></tr>";
-                }
-                ?>
-            </tbody>
-        </table>
+        
+        <table id="sectionsTable" class="display">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Designation</th>
+                <th>Description</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody></tbody> 
+    </table>
         </div>
 
 <?php
 
 include("footer.php");
 ?>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+<link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
+<script>
+$(document).ready(function() {
+    $('#sectionsTable').DataTable({
+        "ajax": {
+            "url": "sections.php?ajax=1",
+            "dataSrc": ""
+        },
+        "columns": [
+            { "data": "id" },
+            { "data": "designation"},
+            { "data": "description" },
+            { 
+                "data": "actions",
+                "orderable": false,
+                "searchable": false,
+                "defaultContent": "" 
+            }
+        ]
+    });
+});
+</script>
